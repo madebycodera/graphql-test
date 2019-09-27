fa52b4c0fab0bd7debbe886b6ba5296d5e342144
Maru-cat <belolutskaya@gmail.com>
2019-09-27 19:16:34 +0300

    - add addTodo, editTodo, deleteTodo Migrations and make them work

--- schema/index.js
+++ schema/index.js
@@ -5,14 +5,7 @@ const pgp = require('pg-promise')();
 const db = {};
 db.conn = pgp(connectionString);
 const {
-    GraphQLObjectType,
-    GraphQLID,
-    GraphQLString,
-    GraphQLScalarType,
-    GraphQLBoolean,
-    GraphQLInt,
-    GraphQLList,
-    GraphQLSchema
+    GraphQLScalarType
 } = graphql;
 const { Kind } = require('graphql/language');
 
@@ -36,32 +29,80 @@ const Date = new GraphQLScalarType({
 
 const Query = {
     todo: (_,{id},ctx) => {
-    const query = `SELECT * FROM "todo" WHERE id=${id}`;
-console.log('todo', id,ctx, query);
-return db.conn.one(query)
-    .then(data => {
-    console.log('then', data);
-return data;
-})
-.catch(err => {
-    console.log('catch', err);
-return 'The error is', err;
-});
-},
-allTodos: () => {
-    const query = `SELECT * FROM "todo"`;
-    console.log('allTodos', query);
-    return db.conn.any(query)
-        .then(data => {
-        console.log('then', data);
-    return data;
-})
-.catch(err => {
-        console.log('catch', err);
-    return 'The error is', err;
-});
-},
+        const query = `SELECT * FROM "todo" WHERE id=${id}`;
+        console.log('todo', id,ctx, query);
+        return db.conn.one(query)
+            .then(data => {
+            console.log('then', data);
+        return data;
+        })
+        .catch(err => {
+            console.log('catch', err);
+        return 'The error is', err;
+        });
+    },
+    allTodos: () => {
+        const query = `SELECT * FROM "todo"`;
+        console.log('allTodos', query);
+        return db.conn.any(query)
+            .then(data => {
+            console.log('then', data);
+            return data;
+        })
+        .catch(err => {
+                console.log('catch', err);
+            return 'The error is', err;
+        });
+    },
 };
+
+const Mutations = {
+    addTodo: (_,args,ctx) => {
+        const query = 'INSERT INTO todo(${this:name}) VALUES(${this:csv}) RETURNING id';
+        console.log('todo', args.todo,ctx, query);
+        return db.conn.query(query, args.todo)
+            .then(data => {
+            console.log('then', data);
+        return data;
+        })
+        .catch(err => {
+            console.log('catch', err);
+        return 'The error is', err;
+        });
+    },
+    editTodo: (_,args,ctx) => {
+        let set = '';
+        const keys = Object.getOwnPropertyNames(args.todo);
+        keys.forEach(function(key,index){
+            if(index === 0) set = set+key+' = \''+args.todo[key]+'\'';
+            else set = set+', '+key+' = \''+args.todo[key]+'\'';
+        });
+        const query = 'UPDATE todo SET '+set+' WHERE id = $1 RETURNING id';
+        console.log('todo',args,ctx, query);
+        return db.conn.query(query, args.id)
+            .then(data => {
+            console.log('then', data);
+            return data;
+        })
+        .catch(err => {
+                console.log('catch', err);
+            return 'The error is', err;
+        });
+    },
+    deleteTodo: (_,args,ctx) => {
+        const query = 'DELETE FROM todo WHERE id = $1 RETURNING id';
+        console.log('todo', args,ctx, query);
+        return db.conn.query(query, args.id)
+        .then(data => {
+            console.log('then', data);
+            return data;
+        })
+        .catch(err => {
+                console.log('catch', err);
+            return 'The error is', err;
+        });
+    }
+}
 
 
 const schemaString = `
@@ -81,11 +122,25 @@ type Todo {
     priority: Int
 }
 
+input AddTodo {
+    description: String
+    createdAt: Date
+    completed: Boolean
+    priority: Int
+}
+
+type Mutation {
+    addTodo(todo: AddTodo!): Todo
+    editTodo(id: ID!, todo: AddTodo): Todo
+    deleteTodo(id: ID!): Todo
+  }
+
 `;
 
 const resolverMap = {
     Date: Date,
-    Query: Query
+    Query: Query,
+    Mutation: Mutations
 };
 
 module.exports = makeExecutableSchema({ typeDefs: schemaString, resolvers: resolverMap });
