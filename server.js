17964d396828cd53535133d10b7959f23cc27424
Maru-cat <belolutskaya@gmail.com>
2019-10-01 17:08:01 +0300

    - remove unneccessary dependencied

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const { ApolloServer } = require('apollo-server');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schema');
 
 const server = new ApolloServer({
